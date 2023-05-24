f28bcf8a8e9b3cf58076ae34b0408e7aec61ad41
Kite-house <hodosok1301@gmail.com>
2023-05-24 20:51:38 +0700

    test

--- buttons/queue_event.js
+++ buttons/queue_event.js
@@ -16,19 +16,20 @@ module.exports = async (client, interaction, db, config) => {
         quantity = results[0].quantity
         limited = results[0].limited
 
-        datetime = new Date().toLocaleString("en-US", {timeZone: "Europe/Moscow"}).split(' ')
-        console.log(datetime[1])
-        if (datetime[2] == 'PM'){
-            hours = parseInt(datetime[1].split(':')[0]) + 12
-            datetime = `${datetime[0]} ${hours}:${datetime[1].split(':')[1]}`
-            console.log('сработало')
+        function datetime(){
+            datetime = new Date().toLocaleString("en-US", {timeZone: "Europe/Moscow"}).split(' ')
+            if (datetime[2]== 'PM'){
+                hours = parseInt(datetime[1].split(':')[0]) + 12
+                datetime = `${datetime[0]} ${hours}:${datetime[1].split(':')[1]}`
+            }
+            else if (datetime[2] == 'AM'){
+                hours = parseInt(datetime[1].split(':')[0])
+                datetime = `${datetime[0]} ${hours}:${datetime[1].split(':')[1]}`
+            }
+            return datetime
         }
-        else if (datetime[2] == 'AM'){
-            hours = parseInt(datetime[1].split(':')[0])
-            datetime = `${datetime[0]} ${hours}:${datetime[1].split(':')[1]}`
-        }
-        
-        console.log(datetime)
+
+        datetime = datetime()
         
         if (datetime >= `${information.split('| ')[1]}, ${time}`){
             row = interaction.message.components[0]
